97f8383d40c5fbeecc2b9e088e5b0e72f37978f3
Okba Nouri <okba.nouri@se.linedata.com>
2018-11-29 12:33:22 +0100

    Family form

--- src/Form/FamilyType.php
+++ src/Form/FamilyType.php
@@ -7,16 +7,18 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class FamilyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titleEn')
-            ->add('titleFr')
-            ->add('descriptionEn')
-            ->add('descriptionFr')
+            ->add('titleEn', TextType::class, array('attr' => array( 'class' => 'form-control')))
+            ->add('titleFr', TextType::class, array('attr' => array( 'class' => 'form-control')))
+            ->add('descriptionEn', TextareaType::class, array('attr' => array( 'class' => 'form-control')))
+            ->add('descriptionFr', TextareaType::class, array('attr' => array( 'class' => 'form-control')))
             ->add('disciplines', CollectionType::class, array(
                 'entry_type' => DisciplineType::class,
                 'allow_add' => true,
