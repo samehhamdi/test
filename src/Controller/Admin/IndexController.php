536153c083757d426acd37bb4d1fefe9db7ba22a
Okba Nouri <okba.nouri@se.linedata.com>
2018-11-30 11:00:39 +0100

    Dashboard

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\FamilyRepository;
+use App\Repository\DisciplineRepository;
+use App\Repository\SkillRepository;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController
@@ -14,9 +16,15 @@ class IndexController extends AbstractController
      * @Route("/admin", name="index")
      */
 
-    public function index(FamilyRepository $familyRepository): Response
+    public function index(FamilyRepository $fR, DisciplineRepository $dR, SkillRepository $sR): Response
     {
-        return $this->render('admin/index/index.html.twig', ['families' => $familyRepository->findAll()]);
+        return $this->render('admin/index/index.html.twig', 
+            array(
+                'families' => $fR->findAll(),
+                'disciplines' => $dR->findAll(),
+                'skills' => $sR->findAll()
+            )
+        );
 
 
     }
