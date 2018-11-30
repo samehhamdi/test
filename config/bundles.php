0e34c01a1201b4ee12e16f21e794090c9392d1ab
STouati <sondes.touati@se.linedata.com>
2018-11-30 17:40:52 +0100

    fix entities api

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
--- src/Entity/Level.php
+++ src/Entity/Level.php
@@ -72,17 +72,7 @@ class Level
         return $this->id;
     }
 
-    public function getLevel(): ?int
-    {
-        return $this->level;
-    }
 
-    public function setLevel(int $level): self
-    {
-        $this->level = $level;
-
-        return $this;
-    }
 
     public function getSkill(): ?Skill
     {
