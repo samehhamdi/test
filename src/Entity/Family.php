fed57af0e2f191a5b619be52af7d935dc130049e
STouati <sondes.touati@se.linedata.com>
2018-11-30 16:03:18 +0100

    fix entities api

--- src/Entity/Family.php
+++ src/Entity/Family.php
@@ -5,7 +5,6 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Validator\Constraints as Assert;
-use ApiPlatform\Core\Annotation\ApiResource;
 
 
 use Doctrine\ORM\Mapping as ORM;
